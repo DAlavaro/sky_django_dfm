46a58530ef3e0d51d515b1f157020a736cc4f2ba
DAlavaro <59216962+DAlavaro@users.noreply.github.com>
2024-05-01 12:49:34 +0300

    21.1

--- app/blog/views.py
+++ app/blog/views.py
@@ -6,8 +6,17 @@ from pytils.translit import slugify
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True)
+
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 class BlogCreateView(CreateView):
     model = Blog
@@ -16,16 +25,18 @@ class BlogCreateView(CreateView):
 
     def form_valid(self, form):
         if form.is_valid():
-            self.object = form.save(commit=False)  # Сохраняем форму, но пока не записываем объект в базу данных
-            self.object.slug = slugify(self.object.title)  # Генерируем slug
-            self.object.save()  # Сохраняем объект с уже сгенерированным slug в базу данных
+            self.object = form.save(commit=False)
+            self.object.slug = slugify(self.object.title)
+            self.object.save()
         return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ['title', 'content', 'photo', 'is_published']
-    success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blog_detail', kwargs={'slug': self.object.slug})
 
 
 class BlogDeleteView(DeleteView):
